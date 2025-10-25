a351a79783c1ae023f046dffed184a64b1d4eac0
playwithduy <playwithduy@gmail.com>
2025-10-25 18:52:21 +0700

    first commit

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -91,6 +91,26 @@ class ProductController {
     return res.status(200).json(order);
   }
 
+
+  async getProductById(req, res, next) {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const { id } = req.params;
+      const product = await Product.findById(id); //req.p 
+      if(!product){
+         return res.status(404).json({ message: 'product not found' });
+      }
+      
+      res.status(200).json(product);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+
   async getProducts(req, res, next) {
     try {
       const token = req.headers.authorization;
